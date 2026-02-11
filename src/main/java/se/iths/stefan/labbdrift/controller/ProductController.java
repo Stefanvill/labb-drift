705a2df9bfb4373725e5d7b96d3ff425c3d02d09
Joel-java-jpg <Joel.schou@iths.se>
2026-02-11 12:27:08 +0100

    Lagt till ProductController och views

---
+++ src/main/java/se/iths/stefan/labbdrift/controller/ProductController.java
@@ -0,0 +1,53 @@
+package se.iths.stefan.labbdrift.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import se.iths.stefan.labbdrift.model.Product;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("products", service.getAll());
+        return "products/list";
+    }
+
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "products/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute Product product) {
+        service.create(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editForm(@PathVariable Long id, Model model) {
+        model.addAttribute("product", service.getOne(id));
+        return "products/edit";
+    }
+
+    @PostMapping("/{id}")
+    public String update(@PathVariable Long id, @ModelAttribute Product product) {
+        service.update(id, product);
+        return "redirect:/products";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable Long id) {
+        service.delete(id);
+        return "redirect:/products";
+    }
+}
